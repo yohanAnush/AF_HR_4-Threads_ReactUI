1c8973e13cf8d3def7aaa74f94e9280fcf0b50ed
littlemzsplendid <tharushi.silva@my.sliit.lk>
2018-06-15 17:51:21 +0530

    develloped department module without search

--- src/components/department/DepartmentListItem.js
+++ src/components/department/DepartmentListItem.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import dateformat from 'dateformat';
 
 export default class DepartmentListItem extends Component {
 
@@ -10,6 +13,19 @@ export default class DepartmentListItem extends Component {
         };
     }
 
+    deleteDepartment(did){
+        let confirmation = window.confirm("Are you sure you want to delete this department?");
+        if(confirmation == true){
+            axios.delete('http://localhost:3001/department/remove/' + did).then(res =>{
+                if(res.status==200) {
+                    this.props.getAllDepartments();
+                }
+            })
+            return true;
+        }
+        return false;
+    }
+
     render() {
         let department = this.state.department;
         return(
@@ -18,7 +34,15 @@ export default class DepartmentListItem extends Component {
                 <td>{ department.name }</td>
                 <td>{ department.description }</td>
                 <td>{ department.department_manager }</td>
-                <td>{ department.date_established }</td>
+                <td>{ dateformat(department.date_established, "yyyy/mm/dd")}</td>
+                <td><button type="button" id={department.did} className="btn btn-danger" onClick={(e) => this.deleteDepartment(department.did)}>
+                        <i className="fas fa-trash-alt"></i>
+                    </button>
+                </td>
+                <td><button type="button" id={department.did} className="btn btn-success">
+                        <Link style={{color: '#FFF'}} to={'edit-department/' + department.did}>Update</Link>
+                    </button>
+                </td>
             </tr>
         );
     }
