3b2f9772d414f44b76147ccb364ee0c986361273
anushka <aliyanage44@gmail.com>
2018-06-15 15:45:56 +0530

    LeaveInsert now lets you search by the employee name and when selected, his/her EID is assigned to the textbox.

--- src/components/leave/LeaveInsert.js
+++ src/components/leave/LeaveInsert.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import dateformat from 'dateformat';
 import axios from 'axios';
 
 import DatePicker from '../commons/DatePicker';
@@ -11,7 +12,8 @@ export default class LeaveInsert extends Component {
             today: '',
             eid: '',
             start: 'Start date',
-            end: 'End date'
+            end: 'End date',
+            value: this.props.value
         };
 
         this.onTextChange = this.props.onTextChange.bind(this);
@@ -25,17 +27,24 @@ export default class LeaveInsert extends Component {
         this.setState({ today: new Date() });
     }
 
+    componentWillReceiveProps(nextProps) {
+        this.setState({ value: nextProps.value });
+    }
+
     onStartDateChange(date) {
-        this.setState({ start: date.toLocaleString() });
+        this.setState({ start: date.toUTCString() });
         console.log(this.state.start);
     }
     onEndDateChange(date) {
-        this.setState({ end: date.toLocaleString() });
+        this.setState({ end: date.toUTCString() });
         console.log(this.state.end);
     }
 
     checkEligibility() {
-        axios.get('http://localhost:8080/leave/emp/' + this.state.eid + '/issue?start=' + + '&end=2018-06-07')
+        axios.get('http://localhost:8080/leave/emp/' + this.state.value + '/issue?start=' + this.state.start + '&end=' + this.state.end)
+            .then(response => {
+                console.log(response);
+            });
     }
 
     render() {
@@ -43,7 +52,7 @@ export default class LeaveInsert extends Component {
             <form className={"form-inline"}>
                 <div className={"form-group row"}>
                     <label htmlFor={"eidForLeave"}>Employee Id(eid)</label>
-                    <input type={"text"} className={"form-control"} id={"eidTxt"} placeholder={"Enter EID"} onChange={this.onTextChange}/>
+                    <input value={this.state.value} type={"text"} className={"form-control"} id={"eidTxt"} placeholder={"Enter EID"} onChange={this.onTextChange}/>
                     <DatePicker name={this.state.start} color={"btn-success"} onDateChange={this.onStartDateChange}/>
                     <DatePicker name={this.state.end} color={"btn-danger"} onDateChange={this.onEndDateChange}/>
                     <button type={"button"} className={"btn btn-md btn-primary"} onClick={this.checkEligibility}>Check</button>
